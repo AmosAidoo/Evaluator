651bdbc6cb9541deca14f5f8165346d104fd475f
AmosAidoo <aidooamos20@gmail.com>
2019-07-12 12:17:33 +0000

    Added two test cases to the driver code

--- src/solve/Main.java
+++ src/solve/Main.java
@@ -3,12 +3,16 @@ package solve;
 public class Main {
 
     public static void main(String[] args) {
-        var x = Evaluator.eval("4*cos(x)", 87);
-        var y = Evaluator.eval("5x + 7*sin(x) + 29", 4);
-        var z = Evaluator.eval("4*cos(2x)*sin(x)", 2);
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(z);
+        var a = Evaluator.eval("4*cos(x)", 87);
+        var b = Evaluator.eval("5x + 7*sin(x) + 29", 4);
+        var c = Evaluator.eval("4*cos(2x)*sin(x)", 2);
+        var d = Evaluator.eval("x^3 + x + 1", 2);
+        var e = Evaluator.eval("e^x + 2*cos(3x) - 1", 3);
+        System.out.println(a); //Output is 0.20934382497177587
+        System.out.println(b); //Output is 49.48829531620888
+        System.out.println(c); //Output is 0.139257933130305
+        System.out.println(d); //Output is 11.0
+        System.out.println(e); //Output is 21.060913604377944
     }
     
 }
