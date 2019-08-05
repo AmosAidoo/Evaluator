6bfc1b1eb95eb378281d1e8620af1bc38ced6a1d
AmosAidoo <aidooamos20@gmail.com>
2019-08-05 15:22:45 +0000

    Added a new test case

--- src/solve/Main.java
+++ src/solve/Main.java
@@ -8,11 +8,14 @@ public class Main {
         var c = Evaluator.eval("4*cos(2x)*sin(x)", 2);
         var d = Evaluator.eval("x^3 + x + 1", 2);
         var e = Evaluator.eval("e^x + 2*cos(3x) - 1", 3);
+        var f = Evaluator.eval("log3(3x)", 15);
+        
         System.out.println(a); //Output is 0.20934382497177587
         System.out.println(b); //Output is 49.48829531620888
         System.out.println(c); //Output is 0.139257933130305
         System.out.println(d); //Output is 11.0
         System.out.println(e); //Output is 21.060913604377944
+        System.out.println(f); //Output is 3.464973520717927
     }
     
 }
